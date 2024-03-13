090073942dfb237a5a191f031905edee24753f30
aleon120894 <user@user>
2024-03-13 19:29:21 +0200

    Reinstall-node-appium-write-first-tests

--- untitled/src/main/java/tests/Login.java
+++ untitled/src/main/java/tests/Login.java
@@ -1,5 +1,10 @@
 package tests;
 
+import io.appium.java_client.AppiumDriver;
+
 public class Login {
     //
+    AppiumDriver driver = new AppiumDriver();
+    Elements elements = new Elements();
+    driver.click(elements.submitButton);
 }
